7d8fdfc4f204252efc6b0d036f94700bbb3302e4
Dave Carlton <davec@mac.com>
2024-12-12 06:20:57 -0600

    "Remove unused dependencies and alias, refactor import path"

--- src/svelte-components/vite.config.ts
+++ src/svelte-components/vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '$lib': path.resolve(__dirname, './src/lib')
-    }
   }
 });
