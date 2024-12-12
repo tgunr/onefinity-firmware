f938ec7762b7697edc598b07d804c487e6d5c01c
Dave Carlton <davec@mac.com>
2024-12-12 10:41:11 -0600

    "Update build script: Copy Vue, Socket.IO, jQuery, Three.js dependencies to build/http/js, bundle JavaScript with browserify, and update index.pug to include required scripts"

--- build.sh
+++ build.sh
@@ -11,15 +11,20 @@ npm install
 # Build the Pug templates
 ./node_modules/.bin/pug -o build/http src/pug
 
-# Bundle the JavaScript with Vue global
-echo "window.Vue = require('vue');" > build/http/js/app.js
+# Copy Vue and other dependencies first
+cp node_modules/vue/dist/vue.js build/http/js/
+cp node_modules/socket.io-client/dist/socket.io.js build/http/js/
+cp node_modules/jquery/dist/jquery.min.js build/http/js/jquery-1.11.3.min.js
+cp node_modules/three/build/three.min.js build/http/js/
+
+# Bundle the JavaScript
 ./node_modules/.bin/browserify \
-  -r vue \
+  src/js/app.js \
   -r ./src/js/api.js:api \
   -r ./src/js/cookie.js:cookie \
   -r ./src/js/sock.js:sock \
   -r semver/functions/lt:semver/functions/lt \
-  src/js/app.js >> build/http/js/app.js
+  -o build/http/js/app.js
 
 # Copy static files
 cp -r src/static/* build/http/
