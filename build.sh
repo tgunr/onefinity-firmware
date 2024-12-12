895f238dfcaec6b8ef690e1eee53f3e9e570da83
Dave Carlton <davec@mac.com>
2024-12-12 10:31:28 -0600

    "Update build script: refactor JavaScript bundling process, create build directory structure, and copy static files. Modify browserify command to include required external modules and alias them for better organization. Improve readability and maintainability."

--- build.sh
+++ build.sh
@@ -6,14 +6,20 @@ npm install
 # Build the Pug templates
 ./node_modules/.bin/pug -o build/http src/pug
 
+# Create build directory structure
+mkdir -p build/http/js
+
 # Bundle the JavaScript
 ./node_modules/.bin/browserify \
-  -x jquery \
-  -x vue \
-  -x socket.io-client \
-  -x clusterize.js \
-  -x three \
-  src/js/app.js -o build/http/js/app.js
+  src/js/app.js \
+  -r ./src/js/api.js:api \
+  -r ./src/js/cookie.js:cookie \
+  -r ./src/js/sock.js:sock \
+  -r semver/functions/lt:semver/functions/lt \
+  -o build/http/js/app.js
+
+# Copy static files
+cp -r src/static/* build/http/
 
 # Set permissions
 sudo chown -R www-data:www-data build/http
