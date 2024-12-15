a2e1bbcc3efe29190b3218cc00c31e9ce02fb987
Dave Carlton <davec@mac.com>
2024-12-15 11:53:05 +0000

    chore: update Dockerfile to install packages and ignore the and install_tools.sh.

--- .devcontainer/install_tools.sh
+++ .devcontainer/install_tools.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 set -e
 
+# Install ARM toolchain
 APT_PACKAGES=(
     "build-essential"
-    "git"
-    "wget"
-    "binfmt-support"
-    "qemu"
-    "gcc-9"
-    "parted"
-    "udev"
-    "zerofree"
+    "gcc"
+    "g++"
     "gcc-avr"
+    "gcc-arm-linux-gnueabihf"
+    "binutils-avr"
     "avr-libc"
     "avrdude"
     "python3"
     "python3-pip"
     "python3-tornado"
-    "inetutils-ping"
     "curl"
     "unzip"
     "python3-setuptools"
-    "gcc-arm-linux-gnueabihf"
     "bc"
-    "vim"
-    "locate"
-    "sudo"
-    "sshpass"
 )
 
-apt-get update
-apt-get upgrade -y
+# Update and install packages
+apt-get -o Acquire::Check-Valid-Until=false update
 apt-get install -y "${APT_PACKAGES[@]}"
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9
-/usr/bin/python3 -m pip install -U yapf
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+
+# Install older yapf version compatible with Debian 9
+/usr/bin/python3 -m pip install 'yapf<0.32.0'
+
+# Setup Node.js
+curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
+
+# Setup compiler alternatives
+# Use actual gcc path instead of symbolic links
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 50
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 50
+
+# Cleanup
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+# Setup SSH
 mkdir -p /root/.ssh
 cat > /root/.ssh/config <<- END
 Host onefinity
